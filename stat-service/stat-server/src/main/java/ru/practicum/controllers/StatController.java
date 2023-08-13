042ddee39a31e8975968f99321649b1ae91d144a
VadimL <badgunn@yandex.ru>
2023-08-13 22:48:05 +0400

    <fix> Диплом "Этап 2". Основной функционал реализован. Устранение ошибок 5

--- stat-service/stat-server/src/main/java/ru/practicum/controllers/StatController.java
+++ stat-service/stat-server/src/main/java/ru/practicum/controllers/StatController.java
@@ -1,7 +1,6 @@
 package ru.practicum.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.StaleObjectStateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
